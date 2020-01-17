f421c57eb826a452dcb32fd1ae3688cb75de9590
BonitaG <bonitaguetta@gmail.com>
2020-01-17 20:49:31 +0600

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,29 @@
+public class Player {
+    private String type;
+    private int year;
+    private  String hight;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getHight() {
+        return hight;
+    }
+
+    public void setHight(String hight) {
+        this.hight = hight;
+    }
+}
